c5595ab7688fbbf094261e4eb6f22b78c17cdb7e
MiguelPacheco7 <miguelpacheco.br@gmail.com>
2024-09-19 16:26:42 -0300

    Finalizando projeto hamburgueria

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'sans':['Roboto', 'sans-serif']
+    },
+    extend: {
+      backgroundImage:{
+        "home": "url('C:\Users\GEAN\Desktop\html\assets\bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
+
